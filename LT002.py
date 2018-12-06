d243775bfe8afb804769d3a92f202a5d4fdfb1d4
Puhan Zhang <pz4ee@virginia.edu>
2018-12-05 23:41:01 -0500

    frommacbook

---
+++ LT002.py
@@ -0,0 +1,14 @@
+class Solution:
+    """
+    @param: n: An integer
+    @return: An integer, denote the number of trailing zeros in n!
+    """
+
+    def trailingZeros(self, n):
+        # write your code here, try to do it without arithmetic operators.
+
+        sum = 0
+        while n != 0:
+            n //= 5
+            sum += n
+        return sum

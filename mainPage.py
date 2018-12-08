227fa49b017c184ba72be3e662055186e8f8c926
Puhan Zhang <puhanzhang@gmail.com>
2018-12-08 16:11:30 -0500

    120820181611

--- mainPage.py
+++ mainPage.py
@@ -1,3 +1,19 @@
-import LT22
+def flatten(nestedList):
+    # Write your code here
+    stack = [nestedList]
+    flatten_list = []
 
-print(LT22.Solution.flatten([[1,1],2,[1,1]]))
+    while stack:
+        top = stack.pop()
+        print(top)
+        if isinstance(top, list):
+            for elem in reversed(top):
+                stack.append(elem)
+                print("+++", elem)
+        else:
+            flatten_list.append(top)
+
+    return flatten_list
+
+
+print(flatten([[1,1],2,[1,2]]))
